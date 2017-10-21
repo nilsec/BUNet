ecd9b10a8efefb0588e3aed4e081510ebaf81c6e
Nils <nils.eckstein@googlemail.com>
2017-10-21 02:43:10 +0200

    Add voxel sizes

--- train.py
+++ train.py
@@ -37,11 +37,13 @@ def train_until(max_iteration):
     request.add(VolumeTypes.GT_MASK, output_size, voxel_size=(40,4,4))
     request.add(VolumeTypes.GT_SCALE, output_size, voxel_size=(40,4,4))
     request.add(VolumeTypes.GT_AFFINITIES, output_size, voxel_size=(40,4,4))
-    
-    snapshot_request = BatchRequest({
-        VolumeTypes.PREDICTED_AFFS: request[VolumeTypes.GT_AFFINITIES],
-        VolumeTypes.LOSS_GRADIENT: request[VolumeTypes.GT_AFFINITIES],
-    })
+
+    snapshot_request = BatchRequest()
+    snapshot_request.add(VolumeTypes.RAW, input_size, voxel_size=(40,4,4))
+    snapshot_request.add(VolumeTypes.PREDICTED_AFFS, output_size, voxel_size=(40,4,4))
+#    snapshot_request.add(VolumeTypes.LOSS_GRADIENT, output_size, voxel_size=(40,4,4))    
+    snapshot_request.add(VolumeTypes.GT_LABELS, output_size, voxel_size=(40,4,4))
+    snapshot_request.add(VolumeTypes.GT_AFFINITIES, output_size, voxel_size=(40,4,4))
 
     data_sources = tuple(
         Hdf5Source(
@@ -50,7 +52,10 @@ def train_until(max_iteration):
                 VolumeTypes.RAW: 'volumes/raw',
                 VolumeTypes.GT_LABELS: 'volumes/labels/neuron_ids_notransparency',
                 VolumeTypes.GT_MASK: 'volumes/labels/mask',
-            }
+            },
+            volume_specs = {VolumeTypes.RAW: VolumeSpec(voxel_size=(40,4,4)),
+                            VolumeTypes.GT_LABELS: VolumeSpec(voxel_size=(40,4,4)),
+                            VolumeTypes.GT_MASK: VolumeSpec(voxel_size=(40,4,4))}
         ) +
         Normalize() +
         RandomLocation() +
@@ -83,7 +88,6 @@ def train_until(max_iteration):
         data_sources +
         RandomProvider() +
         GrowBoundary(steps=4, only_xy=True) +
-        SplitAndRenumberSegmentationLabels() +
         AddGtAffinities([[-1,0,0], [0,-1,0], [0,0,-1]]) +
         BalanceLabels({
                 VolumeTypes.GT_AFFINITIES: VolumeTypes.GT_SCALE
@@ -107,23 +111,17 @@ def train_until(max_iteration):
             gradients={
                 net_io_names['affs']: VolumeTypes.LOSS_GRADIENT
             }) +
-        PrintProfilingStats(every=10)
-    )
-    """ 
+        PrintProfilingStats(every=10) +
         Snapshot({
                 VolumeTypes.RAW: 'volumes/raw',
                 VolumeTypes.GT_LABELS: 'volumes/labels/neuron_ids',
                 VolumeTypes.GT_AFFINITIES: 'volumes/labels/affinities',
                 VolumeTypes.PREDICTED_AFFS: 'volumes/labels/pred_affinities',
-                VolumeTypes.LOSS_GRADIENT: 'volumes/loss_gradient',
             },
             every=100,
-            output_filename='./models/bunet_logs/run_10/batch_{iteration}.hdf',
-            additional_request=snapshot_request) +
-        PrintProfilingStats(every=10)
-        
+            output_filename='run_1/batch_{iteration}.hdf',
+            additional_request=snapshot_request)
     )
-    """
 
     print("Starting training...")
     with build(train_pipeline) as b:
