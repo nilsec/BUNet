36d2badf82b06aa8eea359e8c220dc97be2d2959
Nils Eckstein <ecksteinn@c04u01.int.janelia.org>
2017-10-31 15:05:35 -0400

    Generalize mknet to arbitrary number of fmaps

--- models/mknet.py
+++ models/mknet.py
@@ -5,16 +5,16 @@ import json
 
 def make(x_dim=268, 
          y_dim=268, 
-         z_dim=21,
+         z_dim=84,
          mc_samples=50,
-         learning_rate=0.1e-4,
+         learning_rate=0.5e-4,
          beta1=0.95,
          beta2=0.999,
          epsilon=1e-8,
-         num_fmaps=6,
+         num_fmaps=12,
          fmap_inc_factor=2, 
          downsample_factors=([1,3,3],[1,3,3],[1,3,3]), 
-         drop_rate=0.05,
+         drop_rate=0.1,
          kernel_prior=None):
 
     raw = tf.placeholder(tf.float32, shape=(z_dim, y_dim, x_dim))
@@ -29,25 +29,28 @@ def make(x_dim=268,
                           kernel_prior=kernel_prior,
                           activation='relu')
 
-    logits = pre_sigmoid_split_conv_layer(f_out_batched,
-                                          drop_rate,
-                                          kernel_prior)
+    logits = pre_sigmoid_split_conv_layer(f_out=f_out_batched,
+                                          drop_rate=drop_rate,
+                                          kernel_prior=kernel_prior,
+					  num_fmaps=num_fmaps)
 
-    logits_with_noise = gaussian_noise_layer(logits, mc_samples)
+    logits_with_noise = gaussian_noise_layer(logits=logits, 
+					     n_samples=mc_samples,
+					     num_fmaps=num_fmaps)
 
     # Note this is the shape before the split layer i.e.
     # the shape will be (bs, num_fmaps=12, z,y,x)
 
     f_out_shape_batched = f_out_batched.get_shape().as_list()
     f_out_shape = f_out_shape_batched[1:] # strip batch dim
-    f_out_shape[0] = 3 # Set f_maps to output feature maps (2 in case of x,y only) 
+    f_out_shape[0] = num_fmaps # Set f_maps to output feature maps (3 in case of x,y,z affs) 
 
-    affs = tf.reshape(tf.sigmoid(logits[:,0:3,:,:,:]), f_out_shape)
+    affs = tf.reshape(tf.sigmoid(logits[:,0:num_fmaps,:,:,:]), f_out_shape)
 
     gt_affs = tf.placeholder(tf.float32, shape=f_out_shape)
     loss_weights = tf.placeholder(tf.float32, shape=f_out_shape)
 
-    sigma = logits[:,3:6,:,:,:]
+    sigma = logits[:,num_fmaps:,:,:,:]
 
     loss = stochastic_loss_layer(logits_with_noise,
                                  gt_affs,
