ae6d80146a485c3b499833360d4ca9d9d6016a65
Nils Eckstein <ecksteinn@c04u01.int.janelia.org>
2017-11-10 15:15:06 -0500

    Add entropy & mean aleatoric to combination

--- combine_predictions.py
+++ combine_predictions.py
@@ -3,7 +3,7 @@ import h5py
 from scipy.special import expit
 import os
 
-def online_variance(prediction_paths, dataset="volumes/labels/pred_affinities"):
+def online_variance(prediction_paths, dataset="volumes/labels/pred_affinities", reshape=None):
     """
     Welford algorithm, numerically stable, see wikipedia 
     "Algorithms for calculating variance"
@@ -17,7 +17,14 @@ def online_variance(prediction_paths, dataset="volumes/labels/pred_affinities"):
         n += 1
         print("{}/{}".format(n, N))
         f = h5py.File(path)
-        x = np.array(f[dataset].value, dtype=np.float32)
+	try:
+            if reshape is not None:
+            	x = np.array(np.reshape(f[dataset].value, reshape), dtype=np.float32)
+	    else:
+	        x = np.array(f[dataset].value, dtype=np.float32)
+ 
+	except KeyError:
+	    print "No dset: ", path
         delta = x - mean
         mean += delta/n
         delta2 = x - mean
@@ -28,17 +35,39 @@ def online_variance(prediction_paths, dataset="volumes/labels/pred_affinities"):
     else:
         return mean, M2/(n-1), M2/n
 
-def combine_predictions(base_dir):
+def combine_predictions(base_dir, output_path):
     dirs = [os.path.join(base_dir, top_dir) for top_dir in os.listdir(base_dir)]
-    predictions = [os.path.join(comb_dir, "sample_A.augmented.0.hdf") for comb_dir in dirs]
-    mean, unbiased, biased = online_variance(predictions[:10])
-    f = h5py.File(os.path.join(base_dir, "combined.hdf"))
-    f.create_dataset("volumes/labels/pred_affinities", data=mean)
-    f.create_dataset("volumes/labels/aleatoric", data=unbiased)
+    predictions = [os.path.join(base_dir + "/p_{}".format(n), 
+		   "sample_C_padded_20160501.aligned.filled.cropped.62:153.hdf") for n in range(30)]
 
-    f_0 = h5py.File(predictions[0])
-    f.create_dataset("volumes/raw", data=f_0["volumes/raw"]) 
+    f = h5py.File(output_path, "w")
     
+    """
+    data_dir = '/groups/saalfeld/home/funkej/workspace/projects/caffe/run/cremi_gunpowder/01_data'
+    sample = 'sample_C_padded_20160501.aligned.filled.cropped.62:153.hdf'
+    f_sample = h5py.File(os.path.join(data_dir, sample), "r")
+    f.create_dataset("volumes/labels/gt_affinities", data=f_sample["volumes/labels/affinities"])
+    """
+    mean, unbiased, biased = online_variance(predictions)
+    
+    f.create_dataset("volumes/labels/mean_pred_affinities", data=mean)
+    mean_entropy = -(mean * np.log(mean) + (1. - mean) * np.log(1. - mean))
+    f.create_dataset("volumes/labels/mean_entropy", data=mean_entropy)
+    f.create_dataset("volumes/labels/var_pred_affinities", data=unbiased)
+
+    mean_ale, unbiased_ale, biased_ale = online_variance(predictions, dataset="volume/labels/sigma", reshape=np.shape(mean))    
+    f.create_dataset("volumes/labels/mean_aleatoric", data=mean_ale)
+    f.create_dataset("volumes/labels/var_aleatoric", data=unbiased_ale)
+
+    f_0 = h5py.File(predictions[0], "r")
+    f.create_dataset("volumes/raw", data=f_0["volumes/raw"])
+    f.create_dataset("volumes/labels/0_pred_affinities", data=f_0["volumes/labels/pred_affinities"])
+
+    data_dir = '/groups/saalfeld/home/funkej/workspace/projects/caffe/run/cremi_gunpowder/01_data'
+    sample = 'sample_C_padded_20160501.aligned.filled.cropped.62:153'
+    f_sample = h5py.File(os.path.join(data_dir, sample), "r")
+    f.create_dataset("volumes/labels/gt_affinities", data=f_sample["volumes/labels/affinities"])
 
 if __name__ == "__main__":
-    combine_predictions("/media/nilsec/Backup/predictions_mc_3")
+    combine_predictions("./predictions/140000", "/groups/saalfeld/home/funkej/nils/combined_predictions/"+\
+			"sample_C_padded_20160501.aligned.filled.cropped.r0.140000.c30.62:153.hdf")
